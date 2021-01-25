7ea4811279946da3782a23337e3838e450fb0d44
Hasan Ata Arslan <77296208+HasanAtaArslan@users.noreply.github.com>
2021-01-25 19:23:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Hasan Ata";
+            customer1.Surname = "Arslan";
+            customer1.CustomerId = 48;
+            customer1.CustomerNumber = 1859;
+            customer1.CustomerAccount = "hasan_ata_arslan@mail.com";
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Yasin";
+            customer2.Surname = "Kara";
+            customer2.CustomerId = 75;
+            customer2.CustomerNumber = 3685;
+            customer2.CustomerAccount = "yasinkara546@mail.com";
+
+            customerManager customerManager = new customerManager();
+            customerManager.ChangeSettings(customer1);
+            customerManager.CloseAccount(customer1);
+            customerManager.DelCustomer(customer1);
+            customerManager.ChangeSettings(customer2);
+            customerManager.CloseAccount(customer2);
+            customerManager.DelCustomer(customer2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ customerManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class customerManager
+    {
+       
+        public void DelCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerId + "Id' li kullanıcı silindi.");
+        }
+        public void ChangeSettings(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerNumber + " numaralı müşterinin ayarları değitirildi.");
+        }
+        public void CloseAccount(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerAccount + " adlı hesap güvenlik sebebiyle kapatıldı.");
+        }
+    }
+}
